ca0432652784053f9e8475ab89dfbe0b526473ca
David Prado <j.david.prado1716@gmail.com>
2025-11-01 23:22:58 -0400

    chore: update non-critical dependencies (feed, next-themes, fast-glob, clsx)

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,12 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  experimental: {
-    outputFileTracingIncludes: {
-      '/articles/*': ['./src/app/articles/**/*.mdx'],
-    },
-  },
+  outputFileTracingIncludes: {
+    '/articles/*': ['./src/app/articles/**/*.mdx'],
+  }
 }
 
 const withMDX = nextMDX({
-  extension: /\.mdx?$/,
   options: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypePrismPlus],
